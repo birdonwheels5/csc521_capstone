bf0821f28a41f86805894105cf3d9bac10679852
Squills <wdow.117@gmail.com>
2017-03-16 14:29:20 -0400

    .

--- login/create.php
+++ login/create.php
@@ -247,7 +247,7 @@
         	<input type="text" name="username" placeholder="Username" required>
         	<input type="password" name="password" placeholder="Password" required>
 			<input type="email" name="email" placeholder="Email" required>
-			<input type="submit" name="submit" value="Login" required>
+			<input type="submit" name="submit" value="Create User" required>
 			<div class="small"><a href="./login/reset_pwd.php">Reset your password</a>, <a href="login.php">Login</a> or <a href="view.php">Continue as Guest</a></div>
 		</form>
 		</div>
