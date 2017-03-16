8f68f39f8f07ad20e82e5fb119b354fb9d1b7056
Squills <wdow.117@gmail.com>
2017-03-16 14:47:36 -0400

    .

--- func/login.php
+++ func/login.php
@@ -447,7 +447,7 @@ function get_user_id_num_by_email($email)
 
 function send_validation_email($user_id_num, $uuid, $email)
 {
-    $s_id = "~S0276910";
+    $s_id = "~S0280512";
     $site_web_address = "weblab.salemstate.edu/$s_id/verification.php?id=$user_id_num&etoken=";
     $site_name = 'Bitquote';
     
@@ -476,7 +476,7 @@ function validate_email_address($user_id_num, $uuid, $url_token)
 
 function send_password_validation_email($user_id_num, $uuid, $email, $new_hashed_password, $reset_pass=false)
 {
-    $s_id = "~S0276910";
+    $s_id = "~S0280512";
     $site_name = 'Bitquote';
     
     if($reset_pass == false)
