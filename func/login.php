d4fece593513b7299e9553f8830bb673ef040653
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-11 16:02:34 -0400

    Potential fix for createuser

--- func/login.php
+++ func/login.php
@@ -28,7 +28,7 @@ function create_user($username, $password, $email, $authority_level)
     $hashed_password = hash("sha512", $password . $salt);
     
     // For some reason this doesn't work after adding the email to the end
-    $insert = "INSERT INTO `" . $GLOBALS['mysql_database'] . "`.`users` (`user_id_num`, `username`, `uuid`, `hashed_password`, `new_hashed_password`, `salt`, `authority_level`, `creation_time`, `last_login`, `email`, `validate`, `session_id`) VALUES (NULL, '$username', '$uuid', '$hashed_password', NULL, '$salt', '$authority_level', '$creation_time', '0', '$email', '0', NULL);";
+    $insert = "INSERT INTO `" . $GLOBALS['mysql_database'] . "`.`users` (`user_id_num`, `username`, `uuid`, `hashed_password`, `new_hashed_password`, `salt`, `authority_level`, `creation_time`, `last_login`, `email`, `validate`, `session_id`) VALUES (0, '$username', '$uuid', '$hashed_password', NULL, '$salt', '$authority_level', '$creation_time', '0', '$email', '0', NULL);";
     
     // Add user to the database
     $result = mysqli_query($con, $insert);
