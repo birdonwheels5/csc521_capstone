eeeb0961dc994bf02c2cc988d5a7373286cea9f6
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-12 10:32:11 -0400

    Added chart

--- index.php
+++ index.php
@@ -6,28 +6,112 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 	    <script type="text/javascript">
-	    
-	        // Refresh all Bitcoin prices and the Twitter feed
-		    
-		    $(function() {
-			    refreshBtcPrice(30);
-		    });
-		    
-		    $(function() {
-			    refreshTwitter(30);
-		    });
-            
-		    function refreshBtcPrice(seconds) {
-			    setInterval(function() {
-				    $('#BtcPrice').load('load_exchanges.php');
-			    }, seconds * 1000)
+
+		// Refresh all Bitcoin prices and the Twitter feed
+
+		$(function() {
+		    refreshBtcPrice(30);
+		});
+
+		$(function() {
+		    refreshTwitter(30);
+		});
+
+		function refreshBtcPrice(seconds) {
+		    setInterval(function() {
+			    $('#BtcPrice').load('load_exchanges.php');
+		    }, seconds * 1000)
+		}
+
+		function refreshTwitter(seconds) {
+		    setInterval(function() {
+			    $('#twitter').load('load_tweets.php');
+		    }, seconds * 1000)
+		}
+
+
+		google.charts.load('current', {packages: ['corechart', 'line']});
+		google.charts.setOnLoadCallback(drawTrendlines);
+
+	        function drawTrendlines() {
+		var data = new google.visualization.DataTable();
+		data.addColumn('number', 'X');
+		data.addColumn('number', 'BTCChina');
+		data.addColumn('number', 'BTC-e');
+		data.addColumn('number', 'Bitfinex');
+		data.addColumn('number', 'Bitstamp');
+		data.addColumn('number', 'Coinbase');
+		data.addColumn('number', 'Huobi');
+		data.addColumn('number', 'Kraken');
+		data.addColumn('number', 'OKCoin');
+
+		var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+		var timespan = 24; // In hours
+
+		$.when($.getJSON('get_price_data.php?span=' + timespan)).done( function(json_data) {
+			for(i = 0; i < timespan; i++) {
+			    data.addRow(
+				[i, parseFloat(json_data.btcchina[0][i]), parseFloat(json_data.btce[0][i]), parseFloat(json_data.bitfinex[0][i]), parseFloat(json_data.bitstamp[0][i]), 
+				    parseFloat(json_data.coinbase[0][i]), parseFloat(json_data.huobi[0][i]), parseFloat(json_data.kraken[0][i]), parseFloat(json_data.okcoin[0][i])]
+			    );
+			}
+			var min_array = [
+			    json_data.btcchina[2].min,
+			    json_data.btce[2].min,
+			    json_data.bitfinex[2].min,
+			    json_data.bitstamp[2].min,
+			    json_data.coinbase[2].min,
+			    json_data.huobi[2].min,
+			    json_data.kraken[2].min,
+			    json_data.okcoin[2].min
+			];
+
+			var max_array = [
+			    json_data.btcchina[2].max,
+			    json_data.btce[2].max,
+			    json_data.bitfinex[2].max,
+			    json_data.bitstamp[2].max,
+			    json_data.coinbase[2].max,
+			    json_data.huobi[2].max,
+			    json_data.kraken[2].max,
+			    json_data.okcoin[2].max
+			];
+
+			var min_price = Math.min.apply(Math, min_array);
+			var max_price = Math.max.apply(Math, max_array);
+
+			var options = {
+		    hAxis: {
+		    title: 'Time (Hours)',
+		    direction: '-1',
+		    gridlines: {count: (timespan - 1)},
+		    viewWindowMode: 'explicit',
+		    viewWindow: {
+			    min: 0,
+			    max: (timespan - 1)
 		    }
-		    
-		    function refreshTwitter(seconds) {
-			    setInterval(function() {
-				    $('#twitter').load('load_tweets.php');
-			    }, seconds * 1000)
-		    }
+		 },
+		    vAxis: {
+			title: 'Bitcoin Price',
+			viewWindowMode: 'explicit',
+			viewWindow: {
+				min: (Math.floor(min_price) - 5),
+				max: (Math.ceil(max_price) + 5)
+			    }
+		    },
+		    colors: ['#AB0D06', '#007329'],
+		    legend: {
+			alignment: 'right',
+			position: 'top',
+			maxLines: '8'
+		    },
+		    colors: ['black', 'red', 'blue', 'green', 'orange', 'purple', 'yellow', 'gray']
+		};
+
+			chart.draw(data, options);
+		});
+	    }
+
 	</script>
 </head>
 
@@ -42,7 +126,11 @@
 			<div class="object shadow" id="BtcPrice">
 				<script type="text/javascript">
 					$('#BtcPrice').load('load_exchanges.php');
-                </script>
+                                </script>
+			</div>
+			
+			<div class="object shadow" id="chart_div">
+				
 			</div>
 		</div>
 		
