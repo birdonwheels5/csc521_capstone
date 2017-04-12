7c24d47ada3a7808063300a685be63a9b8d2befb
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-12 11:18:23 -0400

    Update general.php

--- func/general.php
+++ func/general.php
@@ -16,11 +16,11 @@ else
     $s_id = "S0280512";
     
     // Weblab configs
-    $GLOBALS['config_dir'] = "../../bitquote/config.txt";
-    $GLOBALS['log_dir'] = "/home/student/$s_id/bitquote/log.txt";
+    //$GLOBALS['config_dir'] = "/home/student/$s_id/bitquote/config.txt";
+    //$GLOBALS['log_dir'] = "/home/student/$s_id/bitquote/log.txt";
     
-    //$GLOBALS['config_dir'] = "../../config.txt";
-    //$GLOBALS['log_dir'] = "../../log.txt";
+    $GLOBALS['config_dir'] = "../../bitquote/config.txt";
+    $GLOBALS['log_dir'] = "../../bitquote/log.txt";
 }
 
 // Load database settings from config file
