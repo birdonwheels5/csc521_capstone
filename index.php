e3aa5ab351f0f7a7fde0b84594da5c18f68c746b
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-14 13:04:00 -0400

    Added preliminary navbar

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
-    <link rel='stylesheet' type="text/css" href="main.css">
+    	<title>Welcome</title>
+    	<link rel='stylesheet' type="text/css" href="main.css">
+	<?php
+		include "login/CookieHandler.php";
+		include "func/login.php";
+	?>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 	    <script type="text/javascript">
@@ -117,6 +121,28 @@
 	    }
 
 	</script>
+	<?php 
+            
+                $cookie_handler = new CookieHandler();
+                $cookie_name = $cookie_handler->get_cookie_name();
+                $cookie_handler->cookie_exists($cookie_name);
+                
+                // Check to see if the cookie exists
+                if($cookie_handler->get_exists())
+                {
+                    $user_cookie = $cookie_handler->get_cookie($cookie_name);
+                    $uuid = $user_cookie->get_uuid();
+                    $session_id = get_session($uuid);
+                    $cookie_handler->validate_cookie($user_cookie, $session_id);
+                    
+                    // So we can personalize the page a little for the user
+                    $user_data = get_user_data($uuid);
+                    
+                    update_last_login($uuid);
+                }
+		
+                print_header($cookie_handler, $cookie_name);
+            ?>
 </head>
 
 <body class="color-0">
