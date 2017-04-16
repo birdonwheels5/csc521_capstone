86491aea893fc9f7905ea95db8ab14d5e09fbb88
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-16 16:58:40 -0400

    Update general.php

--- func/general.php
+++ func/general.php
@@ -160,7 +160,7 @@ function print_header($cookie_handler, $cookie_name)
     
         
         <ul class="topnav">
-            <li><class ="active" a href ="' . $GLOBALS['student_id'] . '/index.php">Home</a></li>';
+            <li class ="active"><a href ="' . $GLOBALS['student_id'] . '/index.php">Home</a></li>';
                 
 		if($cookie_handler->get_exists())
 		{
