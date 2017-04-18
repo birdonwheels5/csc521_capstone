eebecd0745af2fb340e02f35cc16a8ef50ac04c9
itz-sauce <m_ciulla@salemstate.edu>
2017-04-18 12:15:50 -0400

    Update scraper.php
    deez nutz v2

--- scraper.php
+++ scraper.php
@@ -1,10 +1,10 @@
 <?php
 $ch = curl_init();
 
-curl_set_opt($ch, CURLOPT_URL, "https://bitcointalk.org/index.php?board=77.0")
+curl_set_opt($ch, CURLOPT_URL, "https://bitcointalk.org/index.php?board=77.0");
 
-$data = curl_exec($ch)
-var_dump($data)
+$data = curl_exec($ch);
+var_dump($data);
   
-curl_close($ch)
+curl_close($ch);
 >
