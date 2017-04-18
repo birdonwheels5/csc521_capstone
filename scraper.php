23f0c3482cd50ba30ef44fb4356b411e4ab34d17
itz-sauce <m_ciulla@salemstate.edu>
2017-04-18 12:17:02 -0400

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,7 +1,7 @@
 <?php
 $ch = curl_init();
 
-curl_set_opt($ch, CURLOPT_URL, "https://bitcointalk.org/index.php?board=77.0");
+curl_setopt($ch, CURLOPT_URL, "https://bitcointalk.org/index.php?board=77.0");
 
 $data = curl_exec($ch);
 var_dump($data);
