516fcb5f898469f9b39c724da971f93a3c0b0aaf
itz-sauce <m_ciulla@salemstate.edu>
2017-04-18 13:47:54 -0400

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -6,7 +6,7 @@ curl_setopt($ch, CURLOPT_URL, "https://bitcointalk.org/index.php?board=77.0");
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 $data = curl_exec($ch);
-//<span id="msg_18575527"><a href="https://bitcointalk.org/index.php?topic=1868785.0">[2017-04-14]How Greg Maxwell Exploited Bitcoin Unlimit
+// ~https://bitcointalk.org/index.php?topic=~
 preg_match_all('~<a href="https://bitcointalk.org/index.php?topic=~', $data, $match);
 $threads['url'] = $match[1];
 print_r($threads['url']);die;
