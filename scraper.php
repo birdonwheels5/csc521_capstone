c638b689409b55dd0c9e819c256c0508dd855d42
itz-sauce <m_ciulla@salemstate.edu>
2017-04-19 12:00:55 -0400

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -5,7 +5,7 @@ $threads = array();
 curl_setopt($ch, CURLOPT_URL, "https://bitcointalk.org/index.php?board=77.0");
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-$data = curl_exec($ch);
+//$data = curl_exec($ch);
 // ~https://bitcointalk.org/index.php?topic=~
 preg_match_all('~<a href="https://bitcointalk.org/index.php?topic=~', $data, $match);
 $threads['url'] = $match[1];
