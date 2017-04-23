05c3074926414f400580f12eb078b24bc15ebf96
Reaper117 <wdow.117@gmail.com>
2017-04-23 15:17:10 -0400

    Update index.php

--- index.php
+++ index.php
@@ -176,9 +176,9 @@
 				<div id="chart_div"></div>
 				
 				<form action="" name="chart" method="get" onkeyup="" class="row chart">
-					<input class="chart" type="number" name="x-axis" min="1">
+					<input class="chart" type="number" name="x-axis" min="1" value="1">
 					<input class="chart" type="radio" name="unit" value="1" checked> Hours
-					<input class="chart" type="radio" name="unit" value="24" checked> Days
+					<input class="chart" type="radio" name="unit" value="24"> Days
 					
 				</form>
 				
