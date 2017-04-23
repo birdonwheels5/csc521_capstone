fdabdce408e1091dc3478b01f9901813ad64f94f
itz-sauce <m_ciulla@salemstate.edu>
2017-04-23 12:15:43 -0400

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -8,11 +8,13 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $data = curl_exec($ch);
 // ~https://bitcointalk.org/index.php?topic=~
-preg_match_all('~(https:\/\/bitcointalk\.org\/index\.php\?topic=\d\d\d\d\d\d\d)\.0..(\[\d\d\d\d-\d\d-\d\d\])~', $data, $match);
+preg_match_all('~(https:\/\/bitcointalk\.org\/index\.php\?topic=.*)\.0..(\[\d\d\d\d-\d\d-\d\d\])(.*)</a>~', $data, $match);
 $threads['url'] = $match[1];
 $threads['date'] = $match[2];
+$threads['title'] = $match[3];
 print_r($threads['url']);
 print_r($threads['date']);
+print_r($threads['title']);
   
 curl_close($ch);
 ?>
