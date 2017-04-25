07fdaeed8d2f420715fb227218e975df37ac3c75
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-25 18:41:23 -0400

    target="_blank"

--- load_bitcointalk.php
+++ load_bitcointalk.php
@@ -40,7 +40,7 @@
         
         // Print the rest of the post and the time since it was posted
         print "<div class='tweet'>";
-        print "<b>$username</b>: $post_title \n<br/> Topic link: <a href=$post_url>$post_url</a> </n><br/> <i> $time_since_post ago </i>";
+        print "<b>$username</b>: $post_title \n<br/> Topic link: <a href=$post_url target="_blank">$post_url</a> </n><br/> <i> $time_since_post ago </i>";
         print "</div>";
         print "<br/><br/>";
     }
