6067b7e5299a7b84bf165e731678fe13bb942609
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-25 12:53:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -129,6 +129,10 @@
 		
 		var min_price = Math.min.apply(Math, min_array);
 		var max_price = Math.max.apply(Math, max_array);
+		
+		console.log(min_array);
+		console.log(max_array);
+		
 
 		// Give a little wiggle room
 		max_price = max_price * 1.05;
