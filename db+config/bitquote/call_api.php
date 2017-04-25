820ef3d99b23aa57f6d108a0a1c9affa4bf0e976
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-25 13:10:24 -0400

    Update call_api.php

--- db+config/bitquote/call_api.php
+++ db+config/bitquote/call_api.php
@@ -97,7 +97,7 @@
     //add forum posts to database
     $threads = array();
     $threads = scrape_bitcointalk();
-    $number_of_threads = get_number_of_threads(); 
+    $number_of_threads = get_number_of_threads($threads); 
 
     for ($f = 0; $f < $number_of_threads; $f++)
     {
