ffc46c57ea9d42b8f376b5d14ec758240d0b7fb2
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-25 17:36:28 -0400

    Update load_bitcointalk.php

--- load_bitcointalk.php
+++ load_bitcointalk.php
@@ -1,5 +1,46 @@
+<link rel='stylesheet' type="text/css" href="main.css">
 <?php
-
-print "It works! (bitcointalk)";
-
+    
+    // When opened, this file will fetch all tweets from the database and print them out to the webpage.
+    // The output is HTML formatted.
+    
+    include "func/scraper.php";
+    
+    // Load database settings from config file
+    $settings = array();
+    $settings = load_config();
+    
+    $mysql_user = $settings[0];
+    $mysql_host = $settings[1];
+    $mysql_pass = $settings[2];
+    $mysql_database = $settings[3];
+    
+    // This is also the size of the array
+    $num_posts = 8;
+    
+    // Establish connection to the database
+    $con = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_database);
+    
+    if (mysqli_connect_errno()) 
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    
+    $database_posts = get_database_forum_posts($con, $num_posts);
+    
+    // List the tweets. The order is taken care of by the database.
+    for($i = 0; $i < $num_posts; $i++)
+    {
+        $time_since_post = time_since($database_posts['timestamp'][$i]);
+        
+        $username = $database_posts['names'][$i];
+        $post_title = $database_posts['title'][$i];
+        
+        // Print the rest of the post and the time since it was posted
+        print "<div class='tweet'>";
+        print "$username: $post_title \n<br/> Topic link: $post_url </n><br/> <i> $time_since_post ago </i>";
+        print "</div>";
+        print "<br/><br/>";
+    }
+    
 ?>
