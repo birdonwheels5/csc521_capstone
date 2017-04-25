e2b10adfdef3bff109579a7c175833681751f47b
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-25 16:53:04 -0400

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -108,33 +108,29 @@ function make_timestamp($timestamp, $date)
   return $tstamp;
 }
 
-function compare_threads($processed_tweets, $database_connection)
+function compare_threads($threads, $database_connection)
 {
-    global $number_of_tweets;
-    
-    $number_of_users = count($processed_tweets);
+    $number_of_threads = count($threads['url']);
     
     $results = array();
     
     for($i = 0; $i < $number_of_users; $i++)
     {
-        $user_array = array();
+        $unique_threads = array();
         
-        for($t = 1; $t < (count($processed_tweets[$i])); $t++) // $t is for tweets
+        // Escaped because the tweets in the database are escaped, and we will be comparing to those
+        $post_url = mysqli_real_escape_string($database_connection, $threads['url'][$i]);
+        $username = $threads['names'][$i];
+        $timestamp = $threads['timestamp'][$i];
+
+        $result = mysqli_query($database_connection, "SELECT post_url FROM Forum_Posts WHERE (post_url='$post_url' AND tstamp=$timestamp)");
+
+        if(mysqli_fetch_array($result) == null)
         {
-            // Escaped because the tweets in the database are escaped, and we will be comparing to those
-            $post_text = mysqli_real_escape_string($database_connection, $processed_tweets[$i][$t]);
-            $username = $processed_tweets[$i][0];
-            $timestamp = substr($processed_tweets[$i][0], 0, 10); // First 10 chars are the timestamp
-            
-            $result = mysqli_query($database_connection, "SELECT post_text FROM Twitter_Posts WHERE (post_text='$post_text' AND tstamp=$timestamp)");
-            
-            if(mysqli_fetch_array($result) == null)
-            {
-                $user_array[0] = $processed_tweets[$i][0]; //username
-                $user_array[$t] = $processed_tweets[$i][$t]; // tweet
-                $results[$i] = $user_array;
-            }
+            $unique_threads['url'][$i] = $threads['url'][$i];
+            $unique_threads['title'][$i] = $threads['title'][$i];
+            $unique_threads['timestamp'][$i] = $threads['timestamp'][$i];
+            $user_array['names'][$i] = $threads['names'][$i];
         }
     }
 }  
