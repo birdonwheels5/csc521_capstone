5093fa86f895237a8e8d3980477cfee336635da0
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-25 18:00:41 -0400

    Added the get_database_forum_posts() function

--- func/scraper.php
+++ func/scraper.php
@@ -143,9 +143,59 @@ function add_post($url, $title, $timestamp, $username, $forum_name, $database_co
     return $result;
 }
 
-function get_database_forum_posts($con, $num_posts)
+/* Retrieves a specified number of forum posts from the database from a specified forum
+ * Similar to get_database_tweets() and get_database_reddit_posts()
+ * 
+ * $database_connection is the result of the mysqli_connect() function.
+ * (Int) post_limit: The number of posts to return.
+ * (String) forum_name: The name of the forum that you want to get the posts from. Currently the only supported forum is Bitcointalk.
+ *
+ * Returns a multidimensional array in the following format:
+ * Position [0] = Post urls;
+ * Position [1] = Usernames;
+ * Position [2] = Titles;
+ * Position [3] = Timestamps;
+ */
+function get_database_forum_posts($database_connection, $post_limit, $forum_name)
 {
-  
+    $result = mysqli_query($database_connection, "SELECT username, post_text, tstamp, post_url FROM Forum_Posts WHERE forum_name=$forum_name ORDER BY tstamp DESC LIMIT $post_limit");
+    
+    // Obtain the number of rows from the result of the query
+    $num_rows = mysqli_num_rows($result);
+            
+    // Will be storing all the rows in here
+    $array_of_rows = array();
+                            
+    // Get all the rows
+    for($i = 0; $i < $num_rows; $i++)
+    {
+        $array_of_rows[$i] = mysqli_fetch_array($result);
+    }
+    $size_of_array_of_rows = $num_rows;
+    
+    $post_urls = array();
+    $usernames = array();
+    $titles = array();
+    $timestamps = array();
+    
+    // Get an array of all values for each field
+    for($i = 0; $i < $size_of_array_of_rows; $i++)
+    {
+        $post_urls[$i] = $array_of_rows[$i]["post_urls"];
+        $usernames[$i] = $array_of_rows[$i]["username"];
+        $titles[$i] = $array_of_rows[$i]["post_text"];
+        $timestamps[$i] = $array_of_rows[$i]["tstamp"];
+    }
+    
+    // Package all user data and return as an array
+    $database_posts = array();
+    
+    $database_posts[0] = $post_urls;
+    $database_posts[1] = $usernames;
+    $database_posts[2] = $post_texts;
+    $database_posts[3] = $timestamps;
+    
+    return $database_posts;
 }
 
 
