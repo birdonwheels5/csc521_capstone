41130564a42e9c981593270bc6e55e2a9db6ae36
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-27 10:53:55 -0400

    Update csc263_vgc.php

--- csc263_vgc.php
+++ csc263_vgc.php
@@ -5,28 +5,9 @@
 		<title>VGC Database</title>
 	    <link rel='stylesheet' type="text/css" href="../main.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <?php include "login/CookieHandler.php";
-              include "func/login.php"; ?>
 	</head>
 	
 	<body>
-	    <?php
-                $cookie_handler = new CookieHandler();
-                $cookie_name = $cookie_handler->get_cookie_name();
-                $cookie_handler->cookie_exists($cookie_name);
-                
-                // Check to see if the cookie exists
-                if($cookie_handler->get_exists())
-                {
-                    $user_cookie = $cookie_handler->get_cookie($cookie_name);
-                    $uuid = $user_cookie->get_uuid();
-                    $session_id = get_session($uuid);
-                    $cookie_handler->validate_cookie($user_cookie, $session_id);
-                }            
-		
-                print_header(null, "");
-            ?>
-		
 		<div class="row center">
 			<div class="col-3 empty"></div>
 			<div class="col-6">
