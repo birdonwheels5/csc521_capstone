d01a4a7f2cf87eeff4531a752b4f24b3ff77aa8e
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-27 10:47:45 -0400

    Create csc263_vgc.php

---
+++ csc263_vgc.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>Logout</title>
+	    <link rel='stylesheet' type="text/css" href="../main.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php include "CookieHandler.php";
+              include "../func/login.php"; ?>
+	</head>
+	
+	<body>
+			<?php
+                $cookie_handler = new CookieHandler();
+                $cookie_name = $cookie_handler->get_cookie_name();
+                $cookie_handler->cookie_exists($cookie_name);
+                
+                // Check to see if the cookie exists
+                if($cookie_handler->get_exists())
+                {
+                    $user_cookie = $cookie_handler->get_cookie($cookie_name);
+                    $uuid = $user_cookie->get_uuid();
+                    $session_id = get_session($uuid);
+                    $cookie_handler->validate_cookie($user_cookie, $session_id);
+                }            
+            ?>
+		
+		<div class="row center">
+			<div class="col-3 empty"></div>
+			<div class="col-6">
+				<div class="object shadow">
+					<center><h1>Logout Error</h1></center>
+					<p>
+					<?php
+                                    
+                                    // Perform logout here
+                                    if($cookie_handler->get_exists())
+                                    {
+                                        $cookie_handler->delete_cookie($cookie_name);
+                                        clear_session($uuid);
+                                        header("location:./../index.php");
+                                    }
+                                    else
+                                    {
+                                        print "Unable to log out because user is not logged in!";
+                                    }
+					?>
+					</p>
+				</div>
+			</div>
+			<div class="col-3 empty">	</div>
+		</div>
+		
+	</body>
+	
+</html>
