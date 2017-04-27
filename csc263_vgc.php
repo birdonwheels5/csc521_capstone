efe3431bfb168975f3957d33842a1d5b2c3568ef
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-27 10:54:18 -0400

    Update csc263_vgc.php

--- csc263_vgc.php
+++ csc263_vgc.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="ISO-8859-1">
 		<title>VGC Database</title>
-	    <link rel='stylesheet' type="text/css" href="../main.css">
+	    <link rel='stylesheet' type="text/css" href="main.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	
