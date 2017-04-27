3f01662cd09d05d05fda2a68d6539552e2ad9781
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-27 11:34:20 -0400

    Update csc263_vgc.php

--- csc263_vgc.php
+++ csc263_vgc.php
@@ -79,7 +79,7 @@
 							"PS4 exclusive games rated 8/10, released between 2015 and 2017", // Rating and year are flexible
 							"All games released by Nintendo in 2016",
 							"Games developed by 343 Industries and Bungie between 200 and 2016 that are first person shooters"
-						]
+						];
 						
 						
 						$num_queries = count($queries);
