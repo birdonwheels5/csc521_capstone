30223f6fc9c8c8f25f4c5c8b186e0b7aad9f657f
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-27 11:14:20 -0400

    Update csc263_vgc.php

--- csc263_vgc.php
+++ csc263_vgc.php
@@ -15,10 +15,73 @@
 					<center><h1>Query VGC Database</h1></center>
 					<p>
 					<?php
-						
-					    	// Code goes here
-						print "Content goes here";
+						// Separate PHP block because this is executed separately from the rest of the code
+						if ($_SERVER["REQUEST_METHOD"] == "POST") 
+						{
+							
+						}
 					?>
+					<?php	
+						// Report all PHP errors
+						ini_set('display_errors', 1);
+						error_reporting(E_ALL);
+
+						$host="127.0.0.1";
+						$username="S0276910";
+						$password="New2017";
+						$database="S0276910";
+
+						// Create a new database connect object
+						$dbcon=new mysqli($host, $username, $password, $database);
+
+					  	// Check connection
+						if($dbcon->connect_error) die ($dbcon->connect_error);
+
+						// Make the database connection globally
+						global $dbcon;
+
+					?>
+
+					<h4>Please choose a query:</h4>
+
+					<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+					<select name="dname">
+
+					<?PHP		
+						$queries = array();
+						$queries = 
+						(
+							0 => "SELECT ";
+							1 => "";
+							2 => "";
+							3 => "";
+							4 => "";
+							5 => "";
+							6 => "";
+							7 => "";
+							8 => "";
+							9 => "";
+							10 => "";
+						);
+						
+						$num_queries = count($queries);
+						
+						// Form SQL query string
+						$query= "SELECT dname FROM department";
+
+						for($i = 0; $i < $num_queries; $i++)
+						{
+							echo "<option>",$dname,"\n";
+							print '<option value="' . $i . '">' . $queries[$i] . '</option>';
+						}
+					?>
+
+					</select>
+
+					<input type="submit" value="Display Employee Details">
+
+					</form>
 					</p>
 				</div>
 			</div>
