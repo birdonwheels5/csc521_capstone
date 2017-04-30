1fd5ba3521cdfd668b2d9181c28489826a02833e
Reaper117 <wdow.117@gmail.com>
2017-04-30 13:39:35 -0400

    Update index.php

--- index.php
+++ index.php
@@ -260,7 +260,7 @@
 				<a href="chart.php"><div class="chart" id="chart_div"></div></a>
 				
 				<form action="javascript:void(0);" onsubmit="update_chart()" name="chart_settings" method="post" onchange="update_chart()" onclick="update_chart()" onkeyup="update_chart()" class="row chart">
-					<input class="chart" type="number" name="timespan" min="1" value="1">
+					<input class="chart" type="number" name="timespan" max="512" min="1" value="1">
 					<input class="chart" type="radio" name="unit" value="Hours"> Hours
 					<input class="chart" type="radio" name="unit" value="Days" checked> Days
 					
