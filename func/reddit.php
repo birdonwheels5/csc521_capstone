825660b2de1c1e6fcfe7e5248d8af29c0b2bb423
itz-sauce <m_ciulla@salemstate.edu>
2017-04-30 13:55:02 -0400

    Update

--- func/reddit.php
+++ func/reddit.php
@@ -7,12 +7,18 @@ class RedditPost
     private $post_url = "";		
     private $OP = "";		
     private $post_text = "";		
+    private $post_number;
     
+    function RedditPost($post_number)
+    {
+        
+    }
+                            
     
     
     function get_json_post($subreddit)
     {
-        $url_text = "https://www.reddit.com/r/$subreddit/new/.json?count=20";
+        $url_text = "https://www.reddit.com/r/$subreddit/new/.json?count=20"
         $url = fopen($url_text, "r");
         
         $json = json_decode(stream_get_contents($url));
