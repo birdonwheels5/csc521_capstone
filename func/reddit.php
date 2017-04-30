005a8c0241a9e2ef26ec1fe75acdaf75bfda160b
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-30 14:21:24 -0400

    Update reddit.php

--- func/reddit.php
+++ func/reddit.php
@@ -2,36 +2,37 @@
 
 class RedditPost		
 {		
-    private $tstamp = "";		
-    private $rp_id = "";		
+    private $tstamp = "";	
     private $post_url = "";		
     private $OP = "";		
     private $post_text = "";		
     private $post_number = 0;
     
-    function RedditPost($post_number)
+    function __construct($subreddit, $post_number)
     {
+        $this->post_number = $post_number;
+        get_json_post($subreddit);
+        
         
     }
                             
     
     
-    function get_json_post($subreddit, $post_number)
+    function get_json_post($subreddit)
     {
         $url_text = "https://www.reddit.com/r/$subreddit/new/.json?count=20";
         $url = fopen($url_text, "r");
         
         $json = json_decode(stream_get_contents($url));
         
-        var_dump($json->{"data"}->{"children"}[this->$post_number]);
+        var_dump($json->{"data"}->{"children"}[$this->post_number]);
         
         print "\n\n";
     }
 
 }
 
-$post = new RedditPost(5);
-$post->get_json_post("bitcoin");
+$post = new RedditPost("bitcoin", 5);
 
 
 
