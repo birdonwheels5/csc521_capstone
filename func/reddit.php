6ba7cbb128cc1149acccd4a480ec15913e09900c
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-30 14:15:46 -0400

    Update reddit.php

--- func/reddit.php
+++ func/reddit.php
@@ -7,30 +7,30 @@ class RedditPost
     private $post_url = "";		
     private $OP = "";		
     private $post_text = "";		
-    private $post_number;
+    private $post_number = 0;
     
-    //function RedditPost($post_number)
-    //{
+    function RedditPost($post_number)
+    {
         
-    //}
+    }
                             
     
     
-    function get_json_post($subreddit)
+    function get_json_post($subreddit, $post_number)
     {
         $url_text = "https://www.reddit.com/r/$subreddit/new/.json?count=20";
         $url = fopen($url_text, "r");
         
         $json = json_decode(stream_get_contents($url));
         
-        var_dump($json->{"data"}->{"children"}[1]);
+        var_dump($json->{"data"}->{"children"}[this->$post_number]);
         
         print "\n\n";
     }
 
 }
 
-$post = new RedditPost();
+$post = new RedditPost(5);
 $post->get_json_post("bitcoin");
 
 
