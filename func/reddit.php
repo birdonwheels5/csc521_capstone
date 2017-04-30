6a1dcd58d5bac67b4dbcfa7a99f3f0e28ea94278
itz-sauce <m_ciulla@salemstate.edu>
2017-04-30 13:42:29 -0400

    Update reddit.php

--- func/reddit.php
+++ func/reddit.php
@@ -1,5 +1,16 @@
 <?php
 
+class Post
+{
+  private $tstamp = "";
+  private $rp_id = "";
+  private $post_url = "";
+  private $OP = "";
+  private $post_text = "";
+  
+}
+
+
 function get_reddit_posts($subreddit)
 {
   
