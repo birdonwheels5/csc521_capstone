6116c186b44301fa2966bfd12dd7edaedf38efb5
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-04-30 13:48:45 -0400

    Update reddit.php

--- func/reddit.php
+++ func/reddit.php
@@ -1,14 +1,28 @@
 <?php
 
-function get_reddit_posts($subreddit)
-{
-  
+class RedditPost		
+{		
+    private $tstamp = "";		
+    private $rp_id = "";		
+    private $post_url = "";		
+    private $OP = "";		
+    private $post_text = "";		
+    
+    
+    
+    function get_post($subreddit)
+    {
+         $url = fopen("https://www.reddit.com/r/bitcoin/new/.json?count=20", "r");
+        
+         $json = json_decode(stream_get_contents($url));
+         var_dump($json);
+    }
+
 }
 
-        $url = fopen("https://cex.io/api/ticker/BTC/USD", "r");
-        
-        $json = json_decode(stream_get_contents($url));
-        var_dump($json);
+RedditPost post = new RedditPost();
+post.get_post();
+
 
 
 ?>
