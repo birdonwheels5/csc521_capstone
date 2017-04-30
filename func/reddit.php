1c8c0a438603b70e2211a86c86536fbf0fe0c5e6
itz-sauce <m_ciulla@salemstate.edu>
2017-04-30 15:35:58 -0400

    Update reddit.php

--- func/reddit.php
+++ func/reddit.php
@@ -6,22 +6,24 @@ class RedditPost
     private $post_url = "";
     private $OP = "";
     private $post_text = "";
+    private $subreddit = "";
     private $post_number = 0;
     
     function __construct($subreddit, $post_number, $is_fetching_new_posts)
     {
         $this->post_number = $post_number;
+        $this->subreddit = $subreddit;
         if($is_fetching_new_posts)
         {
-            $this->get_json_post($subreddit);
+            $this->get_json_post();
         }
         
         
     }
     
-    function get_json_post($subreddit)
+    function get_json_post()
     {
-        $url_text = "https://www.reddit.com/r/$subreddit/new/.json?count=20";
+        $url_text = "https://www.reddit.com/r/$this->subreddit/new/.json?count=20";
         $url = fopen($url_text, "r");
         
         $json = json_decode(stream_get_contents($url));
@@ -52,9 +54,14 @@ class RedditPost
         return $this->post_text;
     }
     
+    function get_subreddit()
+    {
+        return $this->subreddit;
+    }
+    
     function add_post($database_connection)
     {
-        $insert = "INSERT INTO Forum_Posts (post_url, post_text, tstamp, OP) VALUES ('$this->post_url', '$this->post_text', '$this->tstamp', '$this->OP');";
+        $insert = "INSERT INTO Forum_Posts (post_url, post_text, tstamp, OP, subreddit) VALUES ('$this->post_url', '$this->post_text', '$this->tstamp', '$this->OP', '$this->subreddit');";
     
         $result = mysqli_query($database_connection, $insert);
         return $result;
