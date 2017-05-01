32eb97d4d6149cc70de80518b1b5eb6a9215c8b5
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2017-05-01 15:21:43 -0400

    Update csc263_vgc.php

--- csc263_vgc.php
+++ csc263_vgc.php
@@ -23,14 +23,14 @@
 					<?PHP		
 						$queries = array();
                         
-                        // This one is special because I'm not gonna write out g.year LIKE %20xx% a million times.
-                        // This would be avoided if our database only stored years for release dates instead of mm/dd/yyyy format.
-                        $query10 = "SELECT Title, Release_Date, Publisher, Genre, Rating FROM Developer as d, Develops as devs, Game as g WHERE (((devs.Developer_ID=1) OR (devs.Developer_ID=4)) AND ";
-                        for($i = 2000; $i <= 2016; $i++)
-                        {
-                            $query10 .= " (g.year LIKE '%$i%')";
-                        }
-                        $query10 .= ");";
+						// This one is special because I'm not gonna write out g.year LIKE %20xx% a million times.
+						// This would be avoided if our database only stored years for release dates instead of mm/dd/yyyy format.
+						$query10 = "SELECT Title, Release_Date, Publisher, Genre, Rating FROM Developer as d, Develops as devs, Game as g WHERE (((devs.Developer_ID=1) OR (devs.Developer_ID=4)) AND ";
+						for($i = 2000; $i <= 2016; $i++)
+						{
+						    $query10 .= " (g.year LIKE '%$i%')";
+						}
+						$query10 .= ");";
                         
 						$queries = 
 						[
@@ -113,6 +113,16 @@
 							global $dbcon;
 							
 							$queries = array();
+                        
+							// This one is special because I'm not gonna write out g.year LIKE %20xx% a million times.
+							// This would be avoided if our database only stored years for release dates instead of mm/dd/yyyy format.
+							$query10 = "SELECT Title, Release_Date, Publisher, Genre, Rating FROM Developer as d, Develops as devs, Game as g WHERE (((devs.Developer_ID=1) OR (devs.Developer_ID=4)) AND ";
+							for($i = 2000; $i <= 2016; $i++)
+							{
+							    $query10 .= " (g.year LIKE '%$i%')";
+							}
+							$query10 .= ");";
+
 							$queries = 
 							[
 								"SELECT comp.Name as CompanyName, cons.Name as ConsoleName FROM Company as comp, Console as cons, Makes as m WHERE ((cons.Console_ID=m.Console_ID) AND (comp.Company_ID=m.Company_ID))",
@@ -123,8 +133,8 @@
 								"",
 								"",
 								"",
-								"",
-								""
+								"SELECT Title, Release_Date, Publisher, Genre, Rating FROM Developer as d, Develops as devs, Game as g WHERE ((devs.Developer_ID=13) AND (g.year LIKE '%2016%'))",
+								$query10
 							];
 							
 							$query_num = $_POST['query_num'];
