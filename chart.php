2b9aed5ac146ec9abfc72e7368b8a8802756e8ec
Reaper117 <wdow.117@gmail.com>
2017-05-01 17:01:10 -0400

    Update chart.php

--- chart.php
+++ chart.php
@@ -41,6 +41,11 @@
 		<div class="object shadow">
 			<div id="chart_div" class="chart">
 			</div>
+			<form action="javascript:void(0);" onsubmit="update_chart()" name="chart_settings" method="post" onchange="update_chart()" onclick="update_chart()" onkeyup="update_chart()" class="row chart">
+				<input class="chart" type="number" name="timespan" min="1" max="365" value="7">
+			    <input class="chart" type="radio" name="unit" value="Hours"> Hours
+			    <input class="chart" type="radio" name="unit" value="Days" checked> Days
+			</form>
 		</div>
 	</div>
 </body>
