39ea26aec5d041af9be8a343707ca6a8accd8a8b
Reaper117 <wdow.117@gmail.com>
2017-05-01 16:28:05 -0400

    Update chart.php

--- chart.php
+++ chart.php
@@ -39,7 +39,11 @@
 	<div class="col-12">
 		<div class="object shadow">
 
-			<div id="chart_div" class="chart"></div>
+			<div id="chart_div" class="chart">
+				<script>
+					drawBitcoinPriceChart(true);
+				</script>
+			</div>
 
 			<form action="javascript:void(0);" onsubmit="update_chart()" name="chart_settings" method="post" onchange="update_chart()" onclick="update_chart()" onkeyup="update_chart()" class="row chart">
 				<input class="chart" type="number" name="timespan" min="1" max="365" value="7">
